d14176341648f61ba3299257589e37e183592550
Swayamx07 <patilswayam1907@gmail.com>
2026-01-07 10:22:38 +0530

    Added basic JavaScript function examples

---
+++ functions.js
@@ -0,0 +1,17 @@
+// Greeting function
+
+function greet(name) {
+    return `Hello, ${name}`;
+}
+console.log(greet("swayam"));
+
+//ADding function
+
+function Add(a, b) {
+    return a + b;
+}
+console.log("Sum", Add(4, 5));
+
+//Arrow function 
+const square = (num) => num * num;
+console.log("Square: ", square(4));
