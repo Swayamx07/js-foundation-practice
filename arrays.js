0bb01892e185d437454899a2a66c55d03f194a6e
Swayamx07 <patilswayam1907@gmail.com>
2026-01-07 10:12:17 +0530

    Added array manipulation programs

--- arrays.js
+++ arrays.js
@@ -1,1 +1,21 @@
+// Program of reverse array
+const arr = [10, 20, 30, 40, 50];
+const reverse = arr.reverse();
+console.log("reversed array:", reverse);
 
+// Program of finding the maximum number
+const numbers = [56, 37, 76, 90, 53];
+let max = numbers[0];
+
+for(let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+        max = numbers[i];
+    }
+}
+
+console.log("Maximum number:", max);
+
+//Even numbers from array
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+const evens = nums.filter(n => n % 2 === 0)
+console.log("Even numbers:", evens);
